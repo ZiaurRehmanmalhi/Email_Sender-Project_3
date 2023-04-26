414e49b0452537909051ba8f84a7b871d6e1f10a
ZiaurRehmanmalhi <ziamalhi1234@gmail.com>
2023-04-26 21:06:32 +0500

    add_text

--- Email_Sender.py
+++ Email_Sender.py
@@ -6,9 +6,10 @@ from sendgrid.helpers.mail import Mail, Email, To, Content
 os.environ['SENDGRID_API_KEY'] = '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'
 
 from_email = Email("ziamalhi1234@gmail.com")
-to_email = To("ziamalhi786@gmail.com")
+to_email = To("ziamalhi786@gmail.com", "abcdef1122ff@gmail.com")
 subject = "Sending email with SendGrid and Python"
-content = Content("text/plain", "Hello, this is a test email sent from Python using the SendGrid API.")
+content = Content("text/plain", """Hi everyone, I am sending this very important 
+email to your from sendgrid but it is just a test to be honest. Ok bye.""")
 
 message = Mail(
     from_email=from_email,
