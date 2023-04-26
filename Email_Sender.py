33d27d972ba11c993493b2f8c28a60e19a20aade
ZiaurRehmanmalhi <ziamalhi1234@gmail.com>
2023-04-26 22:07:24 +0500

    make_input

--- Email_Sender.py
+++ Email_Sender.py
@@ -2,27 +2,36 @@ import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail, Email, To, Content
 
-
 os.environ['SENDGRID_API_KEY'] = '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'
 
-from_email = Email("myemail@gmail.com")
-to_email = To("person1@gmail.com", "person1@gmail.com")
-subject = "Sending email with SendGrid and Python"
-content = Content("text/plain", """Hi everyone, I am sending this very important 
-email to your from sendgrid but it is just a test to be honest. Ok bye.""")
 
-message = Mail(
-    from_email=from_email,
-    to_emails=to_email,
-    subject=subject,
-    plain_text_content=content
-)
+def send_email(content, emails):
+    from_email = Email("ziamalhi1234@gmail.com")
+    to_emails = To(emails.split(','))
 
-try:
-    sg = SendGridAPIClient()
-    response = sg.send(message)
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
-except Exception as e:
-    print(e.message)
+    subject = "Email from SendGrid and Python"
+    plain_text_content = Content("text/plain", content)
+
+    message = Mail(
+        from_email=from_email,
+        to_emails=to_emails,
+        subject=subject,
+        plain_text_content=plain_text_content
+    )
+
+    try:
+        sg = SendGridAPIClient()
+        response = sg.send(message)
+
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+
+    except Exception as e:
+        print(e.message)
+
+
+content = input("Enter your email content: ")
+emails = input("Enter the email addresses separated by commas: ")
+
+send_email(content, emails)
